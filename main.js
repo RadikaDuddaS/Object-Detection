15e966586356416bd3ddd64de3ae369603d1d69d
RadikaDuddaS <102700155+RadikaDuddaS@users.noreply.github.com>
2022-05-05 18:57:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function setup()
     canvas = createCanvas(640,420);
     canvas.center()
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    document.getElementById("Status").innerHTML = "Status : Detecting Objects";
 };
 
 function draw()
